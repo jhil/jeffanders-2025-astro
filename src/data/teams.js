aadad26f81f49f5f62d983ba5a01cb680fa6dc4d
Jeff Anders <jahilnbrand@gmail.com>
2025-02-01 02:24:14 -0700

    teams section

---
+++ src/data/teams.js
@@ -0,0 +1,72 @@
+export const teams = [
+	{
+		image: "/src/images/teams/ambrook-icon.png",
+		name: "Ambrook",
+		role: "Co-Founder",
+		url: "https://ambrook.com?ref=jeffanders.co",
+	},
+	{
+		image: "/src/images/teams/scale-icon.png",
+		name: "Scale AI",
+		role: "Design Manager",
+		url: "https://scale.com/",
+	},
+	{
+		image: "/src/images/teams/meta-icon.png",
+		name: "Meta",
+		role: "Product Designer",
+		url: "https://design.facebook.com/",
+	},
+	{
+		image: "/src/images/teams/minimill-icon.png",
+		name: "Minimill",
+		role: "Co-Founder",
+		url: "https://minimill.co/",
+	},
+	{
+		image: "/src/images/teams/autodesk-icon.png",
+		name: "Autodesk",
+		role: "Product Intern",
+		url: "https://autodesk.com/",
+	},
+	{
+		image: "/src/images/teams/venmo-icon.png",
+		name: "Venmo",
+		role: "Design Intern",
+		url: "https://venmo.com/",
+	},
+	{
+		image: "/src/images/teams/interact-icon.png",
+		name: "Interact",
+		role: "Design Lead",
+		url: "https://joininteract.com/",
+	},
+	{
+		image: "/src/images/teams/bitcamp-icon.png",
+		name: "Bitcamp",
+		role: "Co-Founder",
+		url: "https://bit.camp/",
+	},
+	{
+		image: "/src/images/teams/startupshell-icon.png",
+		name: "Startup Shell",
+		role: "Director",
+		url: "https://startupshell.org/",
+	},
+	{
+		image: "/src/images/teams/hyperion-icon.png",
+		name: "Hyperion",
+		role: "Engineering Intern",
+	},
+	{
+		image: "/src/images/teams/jhilmediadesign-icon.png",
+		name: "JHil Media Design",
+		role: "Founder",
+	},
+	{
+		image: "/src/images/teams/umd-icon.png",
+		name: "U of Maryland",
+		role: "Alum",
+		url: "https://umd.edu"
+	},
+];
