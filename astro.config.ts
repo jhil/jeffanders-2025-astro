746f3a1b5d072becfe4a0beed787be4bdfe4ceac
Jeff Anders <jahilnbrand@gmail.com>
2025-02-01 03:02:16 -0700

    configure projects

--- astro.config.ts
+++ astro.config.ts
@@ -2,13 +2,14 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI
     ? "https://astro-shadcn-ui-template.vercel.app"
     : "http://localhost:4321",
-  integrations: [react()],
+  integrations: [react(), mdx()],
   vite: {
     plugins: [tailwindcss()],
   },
