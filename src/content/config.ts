a5a3ab937d1151d5d3d3306081d4d281f6a62416
Jeff Anders <jahilnbrand@gmail.com>
2025-02-01 15:16:29 -0700

    format

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,15 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const projectsCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    brief: z.string(),
-    date: z.string(),       // You can also use z.date() if you prefer
-    thumbnail: z.string(),
-    external: z.string().optional(),
-  }),
+	schema: z.object({
+		title: z.string(),
+		brief: z.string(),
+		date: z.string(), // You can also use z.date() if you prefer
+		thumbnail: z.string(),
+		external: z.string().optional(),
+	}),
 });
 
 export const collections = {
-  projects: projectsCollection,
+	projects: projectsCollection,
 };
