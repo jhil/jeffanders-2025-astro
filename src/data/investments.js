d7c53561aa09ab9e6d530c5f93398f3d42b8cf73
Jeff Anders <jahilnbrand@gmail.com>
2025-02-01 23:12:30 -0700

    more posts

---
+++ src/data/investments.js
@@ -0,0 +1,82 @@
+export const investments = [
+	{
+		company: "Roboflow",
+		cover: "roboflow.png",
+		url: "https://roboflow.com/",
+	},
+	{
+		company: "HeadsUp",
+		cover: "headsup.png",
+		url: "https://headsup.ai/",
+		label: "acq Hightouch",
+	},
+	{
+		company: "Rome",
+		cover: "rome.png",
+		url: "https://www.rome.ai/",
+	},
+	{
+		company: "Satsuma",
+		cover: "satsuma.png",
+		url: "https://www.alchemy.com/",
+		label: "acq Alchemy",
+	},
+	{
+		company: "Stealth",
+		cover:
+			"https://em-content.zobj.net/source/apple/391/anatomical-heart_1fac0.png",
+	},
+	{
+		company: "Monterey",
+		cover: "monterey.png",
+		url: "https://www.monterey.ai/",
+		label: "acq Reforge",
+	},
+	{
+		company: "Tenfold",
+		cover: "tenfold.png",
+		url: "https://tenfold.xyz/",
+	},
+	{
+		company: "Interbloom",
+		cover: "interbloom.png",
+		url: "https://interbloom.com/",
+	},
+	{
+		company: "Galileo",
+		cover: "galileo.png",
+		url: "https://usegalileo.ai/",
+	},
+	{
+		company: "Applied Labs",
+		cover: "appliedlabs.png",
+		url: "https://appliedlabs.ai/",
+	},
+	{
+		company: "Obviant",
+		cover: "obviant.png",
+		url: "https://www.obviant.com/",
+	},
+	{
+		company: "Reeva",
+		cover: "reeva.png",
+		url: "https://reeva.ai/",
+	},
+	{
+		company: "Notes.fm",
+		cover: "notesfm.png",
+		url: "https://notes.fm/",
+	},
+	{
+		company: "Homeboard",
+		cover: "homeboard.png",
+	},
+	{
+		company: "Stealth",
+		cover: "https://em-content.zobj.net/source/apple/391/credit-card_1f4b3.png",
+	},
+	{
+		company: "Stealth",
+		cover: "https://em-content.zobj.net/source/apple/391/high-voltage_26a1.png",
+	},
+];
