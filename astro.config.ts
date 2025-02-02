27662c4e2754709b379b8cc797eb91df7b065aac
Jeff Anders <jahilnbrand@gmail.com>
2025-02-02 00:21:53 -0700

    netlify

--- astro.config.ts
+++ astro.config.ts
@@ -6,13 +6,18 @@ import mdx from "@astrojs/mdx";
 
 import icon from "astro-icon";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
 	site: process.env.CI
 		? "https://astro-shadcn-ui-template.vercel.app"
 		: "http://localhost:4321",
+
 	integrations: [react(), mdx(), icon()],
 	vite: {
 		plugins: [tailwindcss()],
 	},
+
+	adapter: netlify(),
 });
