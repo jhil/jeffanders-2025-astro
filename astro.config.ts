98a8cef9b8abc6b2ae599ac16044face54fb86e9
Jeff Anders <jahilnbrand@gmail.com>
2025-02-02 00:26:10 -0700

    oop

--- astro.config.ts
+++ astro.config.ts
@@ -12,7 +12,9 @@ import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-	"http://localhost:4321",
+	site: process.env.CI
+		? "https://astro-shadcn-ui-template.vercel.app"
+		: "http://localhost:4321",
 
 	integrations: [react(), mdx(), icon(), robotsTxt()],
 	vite: {
