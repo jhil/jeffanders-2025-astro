6e6bc059c979c7e6476b2fc36c10e3cf91d144e3
Jeff Anders <jahilnbrand@gmail.com>
2025-02-01 20:40:06 -0700

    start lists content

--- src/content/content.config.ts
+++ src/content/content.config.ts
@@ -1,13 +1,24 @@
 import { defineCollection, z } from "astro:content";
 
+const commonSchema = {
+	title: z.string(),
+	date: z.date(),
+};
+
 const projects = defineCollection({
 	schema: z.object({
-		title: z.string(),
+		...commonSchema,
 		brief: z.string(),
-		date: z.date(),
-		thumbnail: z.string(),
+		cover: z.string(),
 		external: z.string().url().optional(),
 	}),
 });
 
-export const collections = { projects };
+const lists = defineCollection({
+	schema: z.object({
+		...commonSchema,
+		icon: z.string(),
+	}),
+});
+
+export const collections = { projects, lists };
