283a708e7cc53e3612a225f91995f496080a6ee9
Jeff Anders <jahilnbrand@gmail.com>
2025-03-01 11:45:27 -0800

    seo

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 
 import icon from "astro-icon";
 
@@ -14,7 +15,18 @@ import robotsTxt from "astro-robots-txt";
 export default defineConfig({
 	site: process.env.CI ? "https://jeffanders.co" : "http://localhost:4321",
 
-	integrations: [react(), mdx(), icon(), robotsTxt()],
+	integrations: [
+		react(),
+		mdx(),
+		icon(),
+		robotsTxt(),
+		partytown({
+			// Adds dataLayer.push as a forwarding-event.
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
+	],
 
 	vite: {
 		plugins: [tailwindcss()],
